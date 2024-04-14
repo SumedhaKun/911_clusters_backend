d7bc67f31e4c5a66c712f6788cde6ca17febc36c
Andrew <arvizumunozandrew@gmail.com>
2024-04-14 15:35:50 -0700

    outline of clustering algorithhm

--- ai/getDBAnalytics.py
+++ ai/getDBAnalytics.py
@@ -1,4 +1,10 @@
 #run clustering algorithm and return cluster information
+import numpy as np
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+
+
 
 #get analytics on a specific case in the DB when provided with the caseNumber
 
