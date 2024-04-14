364621a177f6b484bb43c262c9a764e092c90cf2
Sumedha Kundurthi <70816655+SumedhaKun@users.noreply.github.com>
2024-04-14 11:04:03 -0700

    remove comments

--- server.py
+++ server.py
@@ -37,36 +37,3 @@ if __name__ == "__main__":
         pass
     finally:
         loop.close()
-
-'''
-document = re.sub(r'\W', ' ', str(X[sen]))
-        
-        # remove all single characters
-        document = re.sub(r'\s+[a-zA-Z]\s+', ' ', document)
-        
-        # Remove single characters from the start
-        document = re.sub(r'\^[a-zA-Z]\s+', ' ', document) 
-        
-        # Substituting multiple spaces with single space
-        document = re.sub(r'\s+', ' ', document, flags=re.I)
-        
-        # Removing prefixed 'b'
-        document = re.sub(r'^b\s+', '', document)
-        
-        # Converting to Lowercase
-        document = document.lower()
-        
-        # Lemmatization
-        document = document.split()
-
-        document = [stemmer.lemmatize(word) for word in document]
-        document = ' '.join(document)
-        
-        documents.append(document)
-
-
-        from sklearn.feature_extraction.text import TfidfVectorizer
-        tfidfconverter1 = TfidfVectorizer(max_features=1500, min_df=5, max_df=0.7, stop_words=stopwords.words('english'))
-        X = tfidfconverter1.fit_transform(documents).toarray()
-
-'''
